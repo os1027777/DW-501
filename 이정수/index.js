17cc9ccd164ecec7b2ee919b30d03c1938a46f8a
Lee jeung soo <110653007+os1027777@users.noreply.github.com>
2023-01-05 17:32:39 +0900

    01-05

---
+++ 이정수/index.js
@@ -0,0 +1,9 @@
+function hi(){
+    for(var i=0;i>100;i++){
+        if(i == i){
+            alert("hi")
+        }
+        return i = 0;
+    }
+    
+}
---
+++ 이정수3/index3.js
@@ -0,0 +1,16 @@
+var $circle = $('.circle');
+
+function moveCircle(e) {
+  TweenLite.to($circle, 0.3, {
+    css: {
+      left: e.pageX,
+      top: e.pageY
+    }
+  });
+}
+
+$(window).on('mousemove', moveCircle);
+
+function hi(){
+  
+}
