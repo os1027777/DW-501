a5970b746786b1ce6906eb6d399d66572eb1fedd
Lee jeung soo <110653007+os1027777@users.noreply.github.com>
2023-01-13 18:04:53 +0900

    01-13
    클릭시 움직임

--- index4.js
+++ index4.js
@@ -1,10 +1,10 @@
         var menu_flag = true;
         $("#menu").click(function(){
           if (menu_flag) {
-              $("#container").animate({"top": "+=1200px"}, "slow");
+              $("#container").animate({"bottom": "-=200px"}, "slow");
             menu_flag = false;
-          } else {
-            $("#container").animate({"bottom": "-=1200px"}, "slow");
+          }else{
+            $("#container").animate({"bottom": "+=200px"}, "slow");
                 menu_flag = true;
           }
         });
